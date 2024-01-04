0620985fc4176726c3162d7a9698e5f6e76a4363
Sush <sushkelly@gmail.com>
2024-01-04 09:14:05 +0000

    FIX: Jaunty button

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,6 @@ module.exports = {
 	title: "Creativity & Code",
 	url: "https://sushkelly.co.uk/",
 	language: "en",
-	theme:"jaunty",
 	description: "Sush's website innit.",
 	author: {
 		name: "Sush Kelly",
