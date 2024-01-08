144078baef436c950d9cc2043482354c45bf86cc
Sush <sushkelly@gmail.com>
2024-01-08 10:39:01 +0000

    UPDATE: fix main overflow.

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Creativity & Code",
 	url: "https://sushkelly.co.uk/",
 	language: "en",
-	description: "Sush's website innit.",
+	description: "Web Design and Digital Services in Birmingham",
 	author: {
 		name: "Sush Kelly",
 		email: "me@sushkelly.co.uk",
